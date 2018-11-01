a8b8fe59fd0b4ceae605a17729df12375f44f3b9
Alexander Ela <ela.alexander@gmail.com>
2018-11-01 14:19:50 -0600

    Set up Redux store

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
-
+import { createStore } from 'redux';
+import { rootReducer } from './reducers';
 import { Provider } from 'react-redux';
 
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && windwo.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(rootReducer, devTools);
+
 const router = (
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>
 	)
 
 ReactDOM.render(router, document.getElementById('root'));
