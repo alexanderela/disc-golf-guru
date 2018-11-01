d233ef2b145c26202bc3a27467dcdb1dc09eb621
Alexander Ela <ela.alexander@gmail.com>
2018-11-01 14:26:42 -0600

    Install react-redux

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { createStore } from 'redux';
 import { rootReducer } from './reducers';
 import { Provider } from 'react-redux';
 
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && windwo.__REDUX_DEVTOOLS_EXTENSION__()
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 const store = createStore(rootReducer, devTools);
 
 const router = (
@@ -21,7 +21,4 @@ const router = (
 
 ReactDOM.render(router, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
