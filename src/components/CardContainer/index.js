c565b34e4de6043bf02bad650bb244c40625b2d7
Alexander Ela <ela.alexander@gmail.com>
2018-11-05 22:31:08 -0700

    Add conditional rendering to CardContainer/Error

--- src/components/CardContainer/index.js
+++ src/components/CardContainer/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './CardContainer.css';
+import CourseInfoCard from '../../containers/CourseInfoCard';
+import Error from '../Error';
 
-const CardContainer = ({favorites}) => {
-	// const golfCourseCards = courses.map(course => {
-	// 	return <CourseCard course={course} courses={courses} key={course.id}/>
-	// })
+const CardContainer = ({ favorites }) => {
+	const favoriteCards = favorites.map(course => {
+		return <CourseInfoCard course={course} key={course.id}/>
+	})
 	return(
 		<div className='CardContainer'>
-			{favorites}
+			{favorites.length ? favoriteCards : <Error message={'You currently have no favorites selected'}/>}
 		</div>
 	)
 }
+
 
 export default CardContainer
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -8,24 +8,46 @@ import { Route, Switch } from 'react-router-dom';
 import Home from '../../components/Home';
 import Error from '../../components/Error';
 import { withRouter } from 'react-router';
+import { connect } from 'react-redux';
 import * as DataCleaner from '../../utilities/DataCleaner.js';
 
 class App extends Component {
 
+  filterFavorites = () => {
+    return this.props.golfCourses.filter((course) => course.isFavorite)
+  }
+
   render() {
+    const { golfCourses } = this.props
+    const filteredFavorites = this.filterFavorites()
+
     return (
       <div className="App">
       	<Header />
       	 <Nav />
 	      <Switch>
 	      	<Route exact path='/' render={() => <Home /> }/>
-	    		<Route path='/findcourses' render={() => <MainPage pageName={'Find A Disc Golf Course'}/> }/>
-          <Route path='/favorites' render={() => <CardContainer favorites={'favorites'}/> }/>
-          <Route path='*' exact={true} render={() => <Error message='The page you are looking for does not exist.'/>}/>
+	    		<Route 
+            path='/findcourses' 
+            render={() => <MainPage 
+            pageName={'Find A Disc Golf Course'}/> }/>
+          <Route 
+            path='/favorites' 
+            render={() => filteredFavorites.length 
+              ? <CardContainer favorites={filteredFavorites} />
+              : <Error message={'You currently have no favorites selected'}/> }/>
+          <Route 
+            path='*' 
+            exact={true} 
+            render={() => <Error message='The page you are looking for does not exist.'/>}/>
         </Switch>
       </div>
     );
   }
 }
 
-export default withRouter(App);
+export const mapStateToProps = ({ golfCourses }) => ({ golfCourses });
+
+export const mapDispatchToProps = (dispatch) => ({})
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
