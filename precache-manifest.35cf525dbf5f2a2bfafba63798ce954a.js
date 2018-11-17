28db3f87714414c7c1bd4c8330b40317496ecd53
Alexander Ela <ela.alexander@gmail.com>
2018-11-16 16:41:04 -0800

    Updates

---
+++ precache-manifest.35cf525dbf5f2a2bfafba63798ce954a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9495e288c3c551b6578cc581880e34b",
+    "url": "/disc-golf-guru/static/media/disc-golf-field-2.a9495e28.jpg"
+  },
+  {
+    "revision": "40f4aef1303f5022d4a8",
+    "url": "/disc-golf-guru/static/js/runtime~main.40f4aef1.js"
+  },
+  {
+    "revision": "5a88a864d03f1049261f",
+    "url": "/disc-golf-guru/static/js/main.5a88a864.chunk.js"
+  },
+  {
+    "revision": "3ef3904d249ba9e0e0fe",
+    "url": "/disc-golf-guru/static/js/1.3ef3904d.chunk.js"
+  },
+  {
+    "revision": "5a88a864d03f1049261f",
+    "url": "/disc-golf-guru/static/css/main.f06c93d8.chunk.css"
+  },
+  {
+    "revision": "5900082e01e5bc650063715ec5eaa0ca",
+    "url": "/disc-golf-guru/index.html"
+  }
+];
